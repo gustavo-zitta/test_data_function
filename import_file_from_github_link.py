27088245327e019b02bf628f5f008785b2569404
Gustavo zitta <113521353+gustavo-zitta@users.noreply.github.com>
2023-08-10 12:30:37 -0300

    Update import_file_from_github_link.py

--- import_file_from_github_link.py
+++ import_file_from_github_link.py
@@ -8,54 +8,44 @@ except ImportError:
     import requests
 
 try:
-    import zipfile
-except ImportError:
-    print("Missing 'zipfile' library. Installing...")
-    os.system('pip install zipfile')
-    import zipfile
-
-try:
     from pathlib import Path
 except ImportError:
     print("Missing 'pathlib' library. Installing...")
     os.system('pip install pathlib')
     from pathlib import Path
 
-def download_data(source: str,
+def download_file(source: str,
                   destination: str,
                   remove_source: bool = True) -> Path:
-    """Downloads a zipped dataset from source and unzips to destination.
+    """Downloads a file from source and saves it to destination.
     ... (rest of your docstring) ...
     """
     # Setup path to data folder
     data_path = Path("data/")
-    image_path = data_path / destination
+    file_path = data_path / destination
 
-    # If the image folder doesn't exist, download it and prepare it...
-    if image_path.is_dir():
-        print(f"[INFO] {image_path} directory exists, skipping download.")
+    # If the file doesn't exist, download it...
+    if file_path.exists():
+        print(f"[INFO] {file_path} already exists, skipping download.")
     else:
-        print(f"[INFO] Did not find {image_path} directory, creating one...")
-        image_path.mkdir(parents=True, exist_ok=True)
+        print(f"[INFO] Did not find {file_path}, downloading...")
+        data_path.mkdir(parents=True, exist_ok=True)
 
         # Download data
-        target_file = Path(source).name
-        with open(data_path / target_file, "wb") as f:
-            request = requests.get(source)
-            print(f"[INFO] Downloading {target_file} from {source} to {data_path}...")
-            f.write(request.content)
+        response = requests.get(source)
+        with open(file_path, "wb") as f:
+            print(f"[INFO] Downloading file from {source} to {file_path}...")
+            f.write(response.content)
 
-        # Unzip DATA
-        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
-            print(f"[INFO] Unzipping {target_file} data to {image_path}...")
-            zip_ref.extractall(image_path)
+        print(f"[INFO] Download completed.")
 
-        # Remove .zip file
+        # Remove the source file
         if remove_source:
-            os.remove(data_path / target_file)
+            print(f"[INFO] Removing the source file: {source}")
+            os.remove(file_path)
 
-    return image_path
+    return file_path
 
 # Example usage
-download_data(source="https://github.com/gustavo-zitta/ML-algorithms-/archive/refs/heads/main.zip",
-              destination="data_from_github")
+download_file(source="https://example.com/yourfile.ext",
+              destination="yourfile.ext")
